bbe44840f60048069a8401a1f6d3192e358d37dc
Vigneshwaran T <vigneshwarant54@gmail.com>
2024-07-25 22:57:49 +0530

    crud operation

--- src/components/crud.jsx
+++ src/components/crud.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Button, EditableText, InputGroup, Toaster} from "@blueprintjs/core";
+import { Button, EditableText, InputGroup, Toaster, Tooltip} from "@blueprintjs/core";
 
 const AppToaster = Toaster.create({
     position: "top"
@@ -41,9 +41,57 @@ export function CRUD(){
                     timeout: 3000,
                     intent: 'success'
                 })
-                setAddUser({})
+                document.getElementById('name').value = ''
+                document.getElementById('email').value = ''
+                document.getElementById('website').value = ''
             })
         }
+    }
+
+    function updateUser(id){
+        const user = userDetails.find((user) => user.id === id);
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`,
+            {
+                method: 'PUT',
+                body: JSON.stringify(user),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8'
+                }
+            }
+        )
+        .then((response) => response.json())
+        .then(json => {
+            AppToaster.show({
+                message: 'User Updated Successfully',
+                timeout: 3000,
+                intent: 'primary'
+            })
+        })
+    }
+
+    function deleteUser(id){
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+            method: 'DELETE'
+        })
+        .then((response) => response.json())
+        .then((json) => {
+           setUserDetails((users) => {
+            return users.filter((user) => user.id !== id)
+           })
+           AppToaster.show({
+            message: 'User Deleted Successfully',
+            timeout: 3000,
+            intent: 'danger'
+        })
+        })
+    }
+
+    function onChangeHandler(id, key, value){
+        setUserDetails((users) => {
+            return users.map((user) => {
+                return user.id === id ? {...user, [key] : value} : user
+            })
+        })
     }
 
     return(
@@ -60,32 +108,42 @@ export function CRUD(){
                 </thead>
                 <tbody>
                     {
-                        userDetails?.map((data) => {
-                            return(
-                                <tr>
-                                    <td>{data.id}</td>
-                                    <td>{data.name}</td>
-                                    <td>
-                                        <EditableText value={data.email}/>
-                                    </td>
-                                    <td>
-                                        <EditableText value={data.website}/>
-                                    </td>
-                                    <td>
-                                        <Button className="me-2" intent="success"> Update </Button>
-                                        <Button className="" intent="danger"> Delete </Button>
-                                    </td>
-                                </tr>
-                            )
-                        })
+                        userDetails.map(data => 
+                            <tr key={data.id}>
+                                <td>
+                                    <Tooltip content={data.id}>
+                                        {data.id}
+                                    </Tooltip>
+                                </td>
+                                <td>
+                                    <Tooltip content={data.name}>
+                                        {data.name}
+                                    </Tooltip>
+                                </td>
+                                <td>
+                                    <Tooltip content={data.email}>
+                                        <EditableText value={data.email} onChange={value => onChangeHandler(data.id, 'email', value)}/>
+                                    </Tooltip>
+                                </td>
+                                <td>
+                                    <Tooltip content={data.website}>
+                                        <EditableText value={data.website} onChange={value => onChangeHandler(data.id, 'website', value)}/>
+                                    </Tooltip>
+                                </td>
+                                <td>
+                                    <Button className="me-2" intent="success" onClick={() => updateUser(data.id)}> Update </Button>
+                                    <Button className="" intent="danger" onClick={() => deleteUser(data.id)}> Delete </Button>
+                                </td>
+                            </tr>
+                        )
                     }
                 </tbody>
                 <tfoot>
                     <tr>
                         <td></td>
-                        <td><InputGroup placeholder="Enter Name" value={addUser.name} name="name" onChange={addUserDetails}/></td>
-                        <td><InputGroup placeholder="Enter Email" value={addUser.email} name="email" onChange={addUserDetails}/></td>
-                        <td><InputGroup placeholder="Enter Website" value={addUser.website} name="website" onChange={addUserDetails}/></td>
+                        <td><InputGroup placeholder="Enter Name" name="name" id="name" onChange={addUserDetails}/></td>
+                        <td><InputGroup placeholder="Enter Email" name="email" id="email" onChange={addUserDetails}/></td>
+                        <td><InputGroup placeholder="Enter Website" name="website" id="website" onChange={addUserDetails}/></td>
                         <td>
                             <Button intent="primary" onClick={addUserToTable}>Add User</Button>
                         </td>
